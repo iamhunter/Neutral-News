f3259cd1b0ab495fed86a6c151819f154106a0d1
iams3cond <andr3whunter@gmail.com>
2017-02-23 01:32:49 -0500

    ⚾️Database and admin Construction

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
 from .models import Article
+from .models import Opinion
+from .models import Truth
+from .models import Author
+from .models import Title
+
+
 
 admin.site.register(Article)
+admin.site.register(Opinion)
+admin.site.register(Truth)
+admin.site.register(Author)
+admin.site.register(Title)
