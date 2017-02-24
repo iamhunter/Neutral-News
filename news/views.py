4e72d69a934fd52b17b8f84688de9673af669c22
iams3cond <andr3whunter@gmail.com>
2017-02-24 11:41:01 -0500

    🔗Article links work

--- news/views.py
+++ news/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Article
 
 
 def index(request):
     return render(request, 'news/home.html')
     
-def post(request, slug):
-    return render(request, 'news/posttemplate.html')
-    return render_to_response('news/posttemplate.html', {
-        'post': get_object_or_404(Blog, slug=slug)
-    })
+def post(request, slug):    
+    return render(request, 'news/posttemplate.html', {'article': get_object_or_404(Article, slug=slug)})
