4cba72dab33de23454bde0ab8954b74d14a7c590
iams3cond <andr3whunter@gmail.com>
2017-03-12 15:54:07 -0400

    📇 Card working

--- news/views.py
+++ news/views.py
@@ -5,7 +5,9 @@ from .models import Article
 
 
 def index(request):
-    return render(request, 'news/home.html')
+    article_list = Article.objects.all()
+    context = {'article_list' : article_list}
+    return render(request, 'news/home.html', context)
     
 def post(request, slug):    
     return render(request, 'news/posttemplate.html', {'article': get_object_or_404(Article, slug=slug)})
