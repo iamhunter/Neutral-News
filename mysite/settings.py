7d74b57a26d9e1fb61f74ad6b8ed5cf09cd5646f
iams3cond <andr3whunter@gmail.com>
2017-03-12 23:18:28 -0400

    ȸ Database Changes for Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,13 +75,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
+# Parse database configuration from $DATABASE_URL
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -137,3 +141,8 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = 'news/uploads/'
 MEDIA_URL = '/media/'
+
+import dj_database_url
+import os
+if os.getcwd() == "/app":
+    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
