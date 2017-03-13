ae32926f7fd70ad262e967d26183f026fd5f9c8f
iams3cond <andr3whunter@gmail.com>
2017-03-12 23:58:53 -0400

    🛑 Static Files should work now

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join('news/static'),
 )
 
 
