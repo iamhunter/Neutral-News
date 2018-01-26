b5ebb69627669178e65dc0cc5b0715f374dca996
Unknown <andr3whunter@gmail.com>
2018-01-26 16:46:13 -0500

    🐥 Changed Some Front-End

--- news/models.py
+++ news/models.py
@@ -8,23 +8,24 @@ import datetime
 class ConservativeAuthor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    
+
     def __str__(self):
         return "%s %s" % (self.first_name, self.last_name)
-        
+
 class LiberalAuthor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    
+
     def __str__(self):
         return "%s %s" % (self.first_name, self.last_name)
-    
+
 class Article(models.Model):
     title_text = models.CharField(max_length=500)
     image = models.ImageField()
     conservative_author = models.ForeignKey(ConservativeAuthor, on_delete=models.CASCADE)
     liberal_author = models.ForeignKey(LiberalAuthor, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=100, unique=True)
+    preview = models.TextField()
     truth = models.TextField()
     conservative_opinion = models.TextField()
     liberal_opinion = models.TextField()
@@ -32,10 +33,9 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title_text
-        
+
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
-        
