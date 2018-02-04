0088fd7b566a0d2a373e2d07ba0d03a7a2888712
Unknown <andr3whunter@gmail.com>
2018-02-04 17:21:26 -0500

    🚢 Removed Some Tests

--- news/tests.py
+++ news/tests.py
@@ -7,65 +7,38 @@ from django.urls import reverse
 from django.test import Client
 from .models import *
 
-class ModelTests(TestCase):
-    def test_was_published_recently_with_future_article(self):
-        """
-        was_published_recently() should return False for articles whose
-        pub_date is in the future.
-        """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_article = Article(pub_date=time)
-        self.assertIs(future_article.was_published_recently(), False)
-
-    def test_was_published_recently_with_old_article(self):
-        """
-        was_published_recently() should return False for articles whose
-        pub_date is older than 1 day.
-        """
-        time = timezone.now() - datetime.timedelta(days=30)
-        old_article = Article(pub_date=time)
-        self.assertIs(old_article.was_published_recently(), False)
-
-    def test_was_published_recently_with_recent_article(self):
-        """
-        was_published_recently() should return True for articles whose
-        pub_date is within the last day.
-        """
-        time = timezone.now() - datetime.timedelta(hours=1)
-        recent_article = Article(pub_date=time)
-        self.assertIs(recent_article.was_published_recently(), True)
-        
+class ModelTests(TestCase):        
     def test_article_str(self):
         """
         Creates an article
         """
         article = Article(title_text="article text")
         self.assertIs(article.__str__(), "article text")
-        
+
     def test_conservativeauthor_str(self):
         """
         Creates a conservative author
         """
         conservative = ConservativeAuthor(first_name="first", last_name="last")
         self.assertIsNotNone(conservative.__str__())
-    
+
     def test_liberalauthor_str(self):
         """
         Creates a liberal author
         """
         liberal = LiberalAuthor(first_name="first", last_name="last")
-        self.assertIsNotNone(liberal.__str__())    
+        self.assertIsNotNone(liberal.__str__())
 class ViewTests(TestCase):
     def setUp(self):
         self.client = Client()
-        
+
 #    def test_index_view(self):
 #        """
 #        Tests the index
 #        """
 #        index_view = self.client.get('')
 #        self.assertEqual(index_view.status_code, 200)
-        
+
     def test_post_view(self):
         """
         Tests Post view. should return a 404
@@ -74,6 +47,3 @@ class ViewTests(TestCase):
         url = reverse("post", args='s')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
-
-        
-        
